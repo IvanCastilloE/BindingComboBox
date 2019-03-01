78a292a36ff7d99c820936f8e1dbb8fce99033ec
alumno <alumno@E522>
2019-03-01 14:20:35 -0700

    Agregar archivos de proyecto.

---
+++ Binding/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Binding
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<Persona> ListaPersonas { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            ListaPersonas = new List<Persona>();
+            ListaPersonas.Add(new Persona("Elver", "Galarga"));
+            ListaPersonas.Add(new Persona("Mary", "Conazo"));
+            this.DataContext = this; //define el contexto de los datos 
+
+        }
+    }
+}
